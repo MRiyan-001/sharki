c4f8fc2dd1cbb6b140d76019657952ab37e50ac1
MRiyan-001 <mrh.world001@gmail.com>
2025-04-25 09:52:56 +0500

    Setting images path

--- src/hompage-components/Hero.jsx
+++ src/hompage-components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="w-full border-b-4 border-t-4 border-yellow-500">
       {/* Hero image 1  */}
       <a href="">
-        <img src="public\main-img\hero-img1.png" alt="Upto 50% OFF Sale" className=""/>
+        <img src="public/main-img/hero-img1.png" alt="Upto 50% OFF Sale" className=""/>
       </a>
     </div>
   );
